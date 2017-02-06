33f0fb09cb323dec3e72c3d41f475dde229109b0
Andrei Nicolae <NicolaeAndreiIulian@gmail.com>
2017-02-06 13:25:02 +0200

    Commit AndreiN 3

--- src/main/java/org/fasttrackit/automation/ChangePasswordPage.java
+++ src/main/java/org/fasttrackit/automation/ChangePasswordPage.java
@@ -2,8 +2,6 @@ package org.fasttrackit.automation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
-
 public class ChangePasswordPage {
     @FindBy (name = "password")
     private WebElement currentPasswordField;
@@ -14,24 +12,18 @@ public class ChangePasswordPage {
     @FindBy (name = "newPasswordRepeat")
     private WebElement repeatPasswordField;
 
-    //@FindBy (xpath = "//*[@id="preferences-win"]/form/div/div/div[2]/div[5]/div/button")
-    //@FindBy (partialLinkText = "save")
-    //@FindBy (xpath = "//*[@id=\"preferences-win\"]//button[normalize-space(text())=\"Save\"]")
-
     @FindBy (xpath = "//*[@id='preferences-win']//button[normalize-space(text())='Save']")
     private WebElement saveBtn;
 
     @FindBy (className = "status-msg")
     private WebElement statusMessage;
 
-
     public void changePassword(String currentPass, String newPass, String repeatPass) {
         currentPasswordField.sendKeys(currentPass);
         newPasswordField.sendKeys(newPass);
         repeatPasswordField.sendKeys(repeatPass);
         saveBtn.click();
- }
-
+    }
 
     public String getStatusMessage() {
         String msg = statusMessage.getText();
