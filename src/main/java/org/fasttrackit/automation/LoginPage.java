159342df316a7fe20db88560b9461c7e60c2b44d
Andrei Nicolae <NicolaeAndreiIulian@gmail.com>
2017-02-06 13:23:15 +0200

    Commit AndreiN 2

--- src/main/java/org/fasttrackit/automation/LoginPage.java
+++ src/main/java/org/fasttrackit/automation/LoginPage.java
@@ -2,22 +2,17 @@ package org.fasttrackit.automation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
 public class LoginPage {
     @FindBy(id = "email")
     public WebElement emailField;
-
     @FindBy(id = "password")
     public WebElement passwordField;
-
     @FindBy(id = "loginButton")
     public WebElement loginBtn;
 
     public void login(String email, String password) {
-       emailField.sendKeys(email);
-       passwordField.sendKeys(password);
-       loginBtn.click();
-
-
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        loginBtn.click();
     }
 }
